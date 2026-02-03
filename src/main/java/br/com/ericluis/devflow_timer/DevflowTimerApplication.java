8aaaa731e166ee567622219c1d8e11bf3d5e3b4b
eric luis <ericluismauricio@gmail.com>
2026-02-03 11:15:14 -0300

    chore:initial Spring Boot setup

---
+++ src/main/java/br/com/ericluis/devflow_timer/DevflowTimerApplication.java
@@ -0,0 +1,13 @@
+package br.com.ericluis.devflow_timer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevflowTimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevflowTimerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ericluis/devflow_timer/DevflowTimerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ericluis.devflow_timer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevflowTimerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
